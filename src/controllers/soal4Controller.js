5b47f17e6ccf82f9ecc8713180c22205bf72322f
FahmiDhika <fahmidhika810@gmail.com>
2024-08-22 07:50:55 +0700

    Modul Praktikum 2

--- src/controllers/soal4Controller.js
+++ src/controllers/soal4Controller.js
@@ -3,28 +3,29 @@ exports.bmi = (req,res) => {
     let bb = Number(req.body.bb)
 
     let tbAfter = tb / 100
-    let bmi = bb / (tbAfter*tbAfter)
+    let bmi = bb / (tbAfter * tbAfter)
 
-    let ket = () => {
-        if(bmi <= 18.5){
-            console.log("Stunting")
-        }
-        else if(bmi<= 24.9){
-            console.log("Normal")
-        }
-        else if(bmi<=29.9){
-            console.log("Kelebihan")
-        }
-        else{
-            console.log("Obesitas")
-        }
+    let ket = "Normal"
+    
+    if(bmi <= 18.5){
+        ket = "Stunting"
     }
+    else if(bmi<= 24.9){
+        ket = "Normal"
+    }
+    else if(bmi<=29.9){
+        ket = "Kelebihan berat Badan"
+    }
+    else{
+        ket = "Obesitas"
+    }
+    
 
     let response = {
         tinggi: tb,
         berat: bb,
         bmi: bmi,
-        keterangan: ket
+        status: ket
     }
     return res.json(response)
 }
