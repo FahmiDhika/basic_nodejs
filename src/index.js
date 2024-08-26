4265bdbe35c17438552a4a984c3fa2d3e6fe2da8
FahmiDhika <fahmidhika810@gmail.com>
2024-08-26 14:53:24 +0700

    Praktikum Modul 2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ const bodyParser = require("body-parser") // memanggil library body-parser
 const cors = require("cors") // memanggil library cors
 const app = express()
 
-const praktikumController = require("./controllers/praktikumController")
-const soal1Controller = require("./controllers/soal1Controller")
-const soal2Controller = require("./controllers/soal2Controller")
-const soal3Controller = require("./controllers/soal3Controller")
-const soal4Controller = require("./controllers/soal4Controller")
-
 // penggunaan body-parser untuk ekstrak data request berformat JSON
 app.use(bodyParser.json())
 
@@ -26,36 +20,11 @@ const soal3Route = require("./routes/soal3Route")
 const soal4Route = require("./routes/soal4Route")
 
 // implementasi routes
-app.use("./basic_nodejs", praktikum)
-app.use("./basic_nodejs", soal1Route)
-app.use("./basic_nodejs", soal2Route)
-app.use("./basic_nodejs", soal3Route)
-app.use("./basic_nodejs", soal4Route)
-
-app.get("/test", praktikumController.test)
-app.get("/profil/", praktikumController.profil)
-app.post("/bujur_sangkar", praktikumController.bujur_sangkar)
-
-// soal 1
-app.post("/kubus", soal1Controller.kubus)
-app.post("/balok", soal1Controller.balok)
-app.post("/tabung", soal1Controller.tabung)
-app.post("/bola", soal1Controller.bola)
-
-// soal 2
-app.get("/convert/celcius/:celcius", soal2Controller.celcius)
-app.get("/convert/reamur/:reamur", soal2Controller.reamur)
-app.get("/convert/kelvin/:kelvin", soal2Controller.kelvin)
-app.get("/convert/fahrenheit/:fahrenheit", soal2Controller.fahrenheit)
-
-// soal 3
-app.get("/convert/decimal/:angka", soal3Controller.decimal)
-app.get("/convert/octal/:angka", soal3Controller.octal)
-app.get("/convert/hexadecimal/:angka", soal3Controller.hexaDecimal)
-app.get("/convert/binary/:angka", soal3Controller.binary)
-
-// soal 4
-app.post("/bmi", soal4Controller.bmi)
+app.use("/praktikum", praktikum)
+app.use("/soal1", soal1Route)
+app.use("/soal2", soal2Route)
+app.use("/soal3", soal3Route)
+app.use("/soal4", soal4Route)
 
 // menjalankan server pada port 4000
 app.listen(4000, () => {
--- src/routes/soal2Route.js
+++ src/routes/soal2Route.js
@@ -5,9 +5,9 @@ app.use(express.json())
 
 const soal2Controller = require("../controllers/soal2Controller")
 
-app.get("/convert/celcius/:celcius")
-app.get("/convert/reamur/:reamur")
-app.get("/convert/kelvin/:kelvin")
-app.get("/convert/fahrenheit/:fahrenheit")
+app.get("/convert/celcius/:celcius", soal2Controller.celcius)
+app.get("/convert/reamur/:reamur", soal2Controller.reamur)
+app.get("/convert/kelvin/:kelvin", soal2Controller.kelvin)
+app.get("/convert/fahrenheit/:fahrenheit", soal2Controller.fahrenheit)
 
 module.exports = app
